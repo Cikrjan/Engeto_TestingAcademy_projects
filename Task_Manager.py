63220c0418b64772edb22daa83f39111489d04af
Cikrjan <cikryt.jan@seznam.cz>
2025-04-15 07:08:04 +0200

    Úprava podmínek u kontroly platného vstupu funkce odstranit_ukol()

--- Task_Manager.py
+++ Task_Manager.py
@@ -49,6 +49,9 @@ def odstranit_ukol():
             nazev_smazaneho_ukolu = ukoly[cislo_ukolu][0]
             ukoly.pop(cislo_ukolu)
             print(f"Úkol {nazev_smazaneho_ukolu} byl odstraněn.\n")
+        else:
+            print("Tento úkol není v seznamu.")
+            odstranit_ukol()
     elif (cislo_ukolu + 1) in range(0, (len(ukoly) + 1)):
         nazev_smazaneho_ukolu = ukoly[cislo_ukolu][0]
         ukoly.pop(cislo_ukolu)
