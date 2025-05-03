40bf970f0135b672f06ff428749a72fc67b7cc61
Cikrjan <cikryt.jan@seznam.cz>
2025-05-03 20:42:03 +0200

    Přidání funkce aktualizovat_ukoly()

--- 02_01-Improved_Task_manager.py
+++ 02_01-Improved_Task_manager.py
@@ -31,12 +31,9 @@ def vytvoreni_tabulky():
 def pridat_ukol():
     Nazev_ukolu = input("Název úkolu: ")
     Popis_ukolu = input("Popis úkolu: ")
-
     conn = pripojeni_db()
     kurzor = conn.cursor()
-
     kurzor.execute("INSERT INTO Ukoly (Nazev_ukolu, Popis_ukolu) Values (%s, %s)", (Nazev_ukolu, Popis_ukolu))
-
     conn.commit()
     kurzor.close()
     conn.close()
@@ -45,7 +42,6 @@ def pridat_ukol():
 def zobrazit_ukoly():
     conn = pripojeni_db()
     kurzor = conn.cursor()
-
     kurzor.execute("SELECT UkolID, Nazev_ukolu, Popis_ukolu, Stav " \
                     "FROM Ukoly" \
                     "WHERE Stav = 'Nezahájeno' OR 'Probíhá'")
@@ -53,9 +49,18 @@ def zobrazit_ukoly():
     for row in kurzor.fetchall():
         stav = "Nezahájeno" if row[3] else "Probíhá"
         print(f"{row[0]} : {row[1]} - {row[2]} ({stav})")
-
     kurzor.close()
     conn.close()
+
+def aktualizovat_ukol():
+    ukolID = int(input("ID úkolu: "))
+    stav = input("Nový stav úkolu (Probíhá, Hotovo): ")
+    conn = pripojeni_db()
+    kurzor = conn.cursor()
+    kurzor.execute("UPDATE Ukoly SET Stav = %s WHERE UkolID = %s", (stav, ukolID))
+    kurzor.close()
+    conn.close()
+
 
 def hlavni_menu():
     while True:
