3f82c01a63e895e0e2465822a2cfd6f445c40970
Cikrjan <cikryt.jan@seznam.cz>
2025-05-15 21:10:49 +0200

    Soubor k vytváření testovacách tabulek

---
+++ 02_02_test_init.py
@@ -0,0 +1,31 @@
+import mysql.connector
+
+def create_test_tables():
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="1111",
+        database="taskmanager_test"
+    )
+    kurzor = conn.cursor()
+
+    kurzor.execute("DROP TABLE IF EXISTS ukoly")
+
+    kurzor.execute("""
+            CREATE TABLE IF NOT EXISTS Ukoly (
+                UkolID INT PRIMARY KEY AUTO_INCREMENT,
+                Nazev_ukolu VARCHAR(50) NOT NULL,
+                Popis_ukolu VARCHAR(50) NOT NULL,
+                Stav VARCHAR(10) DEFAULT "Nezahájeno",
+                Datum_vytvoreni DATE
+            )
+            """)
+    
+    conn.commit()
+    kurzor.close()
+    conn.close()
+    print("Testovací tabulky vytvořeny.")
+
+
+if __name__ == "__main__":
+    create_test_tables()
