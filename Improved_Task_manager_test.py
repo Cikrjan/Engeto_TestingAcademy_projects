81f86998e577a1c38a984b547249e78d6d6c14ef
Cikrjan <cikryt.jan@seznam.cz>
2025-05-24 11:35:29 +0200

    Improved_Task_manager_test: uprava test_pridat_ukol_pozitivni()

--- Improved_Task_manager_test.py
+++ Improved_Task_manager_test.py
@@ -1,14 +1,14 @@
 import pytest
 import mysql.connector
 from test_init import vytvoreni_test_tabulky
-from 02_01-Improved_Task_manager import
+from Improved_Task_manager import pridat_ukol, aktualizovat_ukol, odstranit_ukol
 
 @pytest.fixture(scope="session", autouse=True)
 def vytvoreni_test_db():
     vytvoreni_test_tabulky()
 
 @pytest.fixture(scope="module")
-def db_connection():
+def pripojeni():
     conn = mysql.connector.connect(
         host = "localhost",
         user = "root",
@@ -18,43 +18,42 @@ def db_connection():
     yield conn
     conn.close()
 
-def test_pridat_ukol_pozitivni(db_connection):
-    kurzor = db_connection.cursor()
-    kurzor.execute("INSERT INTO ukoly (Nazev_ukolu, Popis_ukolu, Datum_vytvoreni) VALUES ('Test_nazev', 'Test_popis', NOW())")
-    db_connection.commit()
+def test_pridat_ukol_pozitivni(pripojeni):
+    kurzor = pripojeni.cursor()
+    pridat_ukol(pripojeni, "Test_nazev", "Test_popis")
     kurzor.execute("SELECT * FROM ukoly WHERE Nazev_ukolu = 'Test_nazev'")
     vysledek = kurzor.fetchone()
     kurzor.execute("DELETE FROM ukoly WHERE Nazev_ukolu = 'Test_nazev'")
-    db_connection.commit()
+    pripojeni.commit()
     kurzor.close()
     
     assert vysledek is not None
 
-def test_pridat_ukol_negativni(db_connection):
-    kurzor = db_connection.cursor()
+def test_pridat_ukol_negativni(pripojeni):
+    kurzor = pripojeni.cursor()
     kurzor.execute("INSERT INTO ukoly (Nazev_ukolu, Popis_ukolu, Datum_vytvoreni) VALUES ('', 'Test_popis', NOW())")
-    db_connection.commit()
+    pripojeni.commit()
     kurzor.execute("SELECT * FROM ukoly WHERE Nazev_ukolu = 'Test_nazev'")
     vysledek = kurzor.fetchone()
     kurzor.execute("DELETE FROM ukoly WHERE Nazev_ukolu = 'Test_nazev'")
-    db_connection.commit()
+    pripojeni.commit()
     kurzor.close()
     
     assert vysledek is None
 
-def test_aktualizace_ukolu_pozitivni(db_connection):
-    kurzor = db_connection.cursor()
+def test_aktualizace_ukolu_pozitivni(pripojeni):
+    kurzor = pripojeni.cursor()
     #Přidání testovacího úkolu
     kurzor.execute("INSERT INTO ukoly (Nazev_ukolu, Popis_ukolu, Datum_vytvoreni) VALUES ('Test_nazev', 'Test_popis', NOW())")
-    db_connection.commit()
+    pripojeni.commit()
     #změna stavu úkolu
     kurzor.execute("UPDATE ukoly SET Stav = 'Probíhá' WHERE UkolID = 1")
-    db_connection.commit()
+    pripojeni.commit()
     #výběr testovacího úkolu
     kurzor.execute("SELECT Stav FROM ukoly WHERE UkolID = 1")
     vysledek = kurzor.fetchone()[0]
     kurzor.execute("DELETE FROM ukoly WHERE Nazev_ukolu = 'Test_nazev'")
-    db_connection.commit()
+    pripojeni.commit()
     kurzor.close()
 
     assert vysledek == "Probíhá"
