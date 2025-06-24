5bdce256341634efa34d2b4663cf62145fe34bf1
Cikrjan <cikryt.jan@seznam.cz>
2025-06-24 06:15:15 +0200

    Přidána testovací funkce pro pozitivní test přihlášení

--- 03_Playwright_tests/test_SignIn.py
+++ 03_Playwright_tests/test_SignIn.py
@@ -2,16 +2,28 @@ import pytest
 from playwright.sync_api import sync_playwright, Page
 
 @pytest.fixture(scope='function', autouse=True)
-def page(request):
+def new_page(request):
     browser_name = request.param
     with sync_playwright() as p:
         browser_type = getattr(p, browser_name)
-        browser = browser_type.launch(headless=True)
+        browser = browser_type.launch(headless=False, slow_mo=500)
         page = browser.new_page()
         yield page
         page.close()
 
 
-@pytest.mark.parametrize("page", ["chromium", "firefox", "webkit"], indirect=True)
-def test_SignIn(page):
-    pass
+@pytest.mark.parametrize("new_page", ["chromium", "firefox", "webkit"], indirect=True)
+def test_SignIn_Positive(new_page):
+    new_page.goto("https://the-internet.herokuapp.com/")
+
+    login_page = new_page.locator("[href='/login']")
+    login_page.click()
+
+    user_name = new_page.locator("input#username").fill("tomsmith")
+    password = new_page.locator("input#password").fill("SuperSecretPassword!")
+
+    login_button = new_page.locator(".radius")
+    login_button.click()
+
+    assert new_page.url == "https://the-internet.herokuapp.com/secure"
+
